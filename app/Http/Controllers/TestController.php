2ee1142aa31c52ef813b0200c84e2bd4add5ff09
dong <dongtx04@gmail.com>
2023-07-25 15:45:08 +0700

    update file

---
+++ app/Http/Controllers/TestController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Exceptions\TestException;
+
+
+
+
+class TestController extends Controller
+{
+    public function Test()
+    {
+//        try {
+            $this->makeSomethingRisky();
+
+//        } catch (TestException $e) {
+//            dd(1);
+//            return response()->json([
+//                'message' => $e->getMessage(),
+//            ], $e->getCode());
+//        };
+
+//        return response()->json([
+//            'message' => 'hi'
+//        ]);
+    }
+
+    protected function makeSomethingRisky()
+    {
+        throw TestException::internalException();
+
+    }
+}
